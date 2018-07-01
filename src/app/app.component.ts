d6d10c6c327d25e65275c13031026a5c5197298c
Christina Chen <cyc25>
2018-07-01 21:59:34 +0200

    fix tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
  
 import { HomePage } from '../pages/home/home';
 import { SplashPage } from '../pages/splash/splash';
+import { TabsPage } from '../pages/tabs/tabs';
  
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
  
-    rootPage:any = HomePage;
+    rootPage:any = TabsPage;
  
     constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, modalCtrl: ModalController) {
  
